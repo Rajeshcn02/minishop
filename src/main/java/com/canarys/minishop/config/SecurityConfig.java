0174d3f8a5e7f22809cf07349c87eb53240ff24b
Rajesh C N <rajeshdevopscn@gmail.com>
2025-11-20 20:57:31 +0530

    Update SecurityConfig.java

--- src/main/java/com/canarys/minishop/config/SecurityConfig.java
+++ src/main/java/com/canarys/minishop/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;  // ✅ FIX
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
